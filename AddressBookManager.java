949e2736bbc7eb405e80c132d716d70596d8191c
Shravyak-Hub <shravyak156@gmail.com>
2020-10-20 22:12:31 +0530

    Modified file: AddressBookManager.java to take input from user

--- AddressBookManager.java
+++ AddressBookManager.java
@@ -1,16 +1,25 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBookManager implements IAddressBook{
 
     public void createPerson() {
-        ArrayList<String> addressBook = new ArrayList<>();
-        addressBook.add("Shravya");
-        addressBook.add("K");
-        addressBook.add("Bangalore");
-        addressBook.add("Bangalore");
-        addressBook.add("Karnataka");
-        addressBook.add("560062");
-        addressBook.add("9591316654");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first name: ");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter last name: ");
+        String lastName = scanner.nextLine();
+        System.out.println("Enter address : ");
+        String address = scanner.nextLine();
+        System.out.println("Enter city: ");
+        String city = scanner.nextLine();
+        System.out.println("Enter state: ");
+        String state = scanner.nextLine();
+        System.out.println("Enter zip: ");
+        String zip = scanner.nextLine();
+        System.out.println("Enter phone number: ");
+        String phoneNumber = scanner.nextLine();
+        new Person(firstName, lastName, address, city, state, zip, phoneNumber);
+        System.out.println("Contact added!");
     }
 
 }
