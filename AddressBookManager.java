1ce6cf2bd9937b4c1ef920ebad09d2d48449c32e
Shravyak-Hub <shravyak156@gmail.com>
2020-10-20 22:06:24 +0530

    Added file: AddressBookManager.java with method to create contact

---
+++ AddressBookManager.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class AddressBookManager implements IAddressBook{
+
+    public void createPerson() {
+        ArrayList<String> addressBook = new ArrayList<>();
+        addressBook.add("Shravya");
+        addressBook.add("K");
+        addressBook.add("Bangalore");
+        addressBook.add("Bangalore");
+        addressBook.add("Karnataka");
+        addressBook.add("560062");
+        addressBook.add("9591316654");
+    }
+
+}
