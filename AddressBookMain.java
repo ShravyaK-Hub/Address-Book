ffbabb839d3d1091aa751babc08a06ea20521c9f
Shravyak-Hub <shravyak156@gmail.com>
2020-10-21 10:10:33 +0530

    Modified file: AddressBookMain.java by allowing user to add or edit contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class AddressBookMain {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Address Book Program");
         AddressBookManager addressBookManager = new AddressBookManager();
-        addressBookManager.createPerson();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("Choose:\n 1.Add person\n 2.Edit person");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+
+                case 1:
+                    addressBookManager.createPerson();
+                    break;
+
+                case 2:
+                    addressBookManager.editPerson();
+                    break;
+
+                default:
+                    System.out.println("Invalid input");
+
+            }
+
+        }
 
     }
 
